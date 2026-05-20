f7cf3ebd2cde373579d4697ea1e1c1e64cdaf289
이하늘 <150312623+smflsmfqh@users.noreply.github.com>
2026-05-20 14:02:57 +0900

    [Add] Basic Health System

--- Assets/Scripts/NPC/FootStompCollider.cs
+++ Assets/Scripts/NPC/FootStompCollider.cs
@@ -7,7 +7,7 @@ public class FootStompCollider : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Debug.Log("[FootStomp] 플레이어 밟힘!");
-            // 추후 other.GetComponent<HealthSystem>().TakeDamage() 연결
+            other.GetComponent<PlayerHealth>().TakeDamage();
         }
     }
 }
