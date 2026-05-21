07232d29d41bfbec4f8e0bb99255baa3b63e14e3
이하늘 <150312623+smflsmfqh@users.noreply.github.com>
2026-05-21 14:40:41 +0900

    [Add] GameOver UI

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
@@ -8,9 +9,14 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private GameObject _hudPanel;
 
+    [SerializeField]
+    private GameObject _gameOverPanel;
+
+    [SerializeField]
+    private TextMeshProUGUI _gameOverDescText;
+
     // --- 추후 여러 판넬 추가할 예정
     // 1. 아내 개미에게 오는 카톡 알림 ui
-    // 2. 게임 오버 판넬
 
     private void Awake()
     {
@@ -26,11 +32,15 @@ public class UIManager : MonoBehaviour
     private void Start()
     {
         ShowHUD();
+        SetPanel(_gameOverPanel, false);
     }
 
-    public void ShowGameOver()
+    public void ShowGameOver(CauseDeath cause)
     {
         SetPanel(_hudPanel, false);
+        SetPanel(_gameOverPanel, true);
+
+        _gameOverDescText.text = StringTableManager.Instance.GetDeathMessage(cause);
     }
 
     public void ShowHUD()
