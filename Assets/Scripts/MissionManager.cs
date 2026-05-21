79cf05e9b4517f71a7a8c52db47dc94ae1912c5c
이하늘 <150312623+smflsmfqh@users.noreply.github.com>
2026-05-21 17:20:31 +0900

    [Add] Mission System

---
+++ Assets/Scripts/MissionManager.cs
@@ -0,0 +1,68 @@
+using System;
+using UnityEngine;
+
+public class MissionManager : MonoBehaviour
+{
+    public static MissionManager Instance { get; private set; }
+
+    [SerializeField]
+    private ItemData[] _missionPool;
+
+    [SerializeField]
+    private float[] _unlockTimes = { 2f, 10f, 20f };
+
+    public event Action<ItemData> OnMissionAssigned;
+    public event Action<ItemData> OnMissionCompleted;
+
+    private float _elapsedTime;
+    private bool[] _assigned;
+    private bool[] _completed;
+    private int _completedCount;
+
+    public bool OnAllMissionCompleted => _completedCount >= _missionPool.Length;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+        _assigned = new bool[_missionPool.Length];
+        _completed = new bool[_missionPool.Length];
+    }
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        for (int i = 0; i < _missionPool.Length; i++)
+        {
+            if (!_assigned[i] && _elapsedTime >= _unlockTimes[i])
+            {
+                _assigned[i] = true;
+                OnMissionAssigned?.Invoke(_missionPool[i]);
+            }
+        }
+    }
+
+    public void ReportCollected(string itemName)
+    {
+        for (int i = 0; i < _missionPool.Length; i++)
+        {
+            Debug.Log("[ReportCollected] 실행!");
+            if (_assigned[i] && !_completed[i] && _missionPool[i].itemName == itemName)
+            {
+                _completed[i] = true;
+                _completedCount++;
+                OnMissionCompleted?.Invoke(_missionPool[i]);
+                Debug.Log("[ReportCollected] 미션 완료!");
+
+                return;
+            }
+        }
+    }
+}
