d99c946e7af8604db9d70836fc74a76fb74fcd74
이하늘 <skyee46@gmail.com>
2026-05-27 10:15:45 +0900

    [Add] Cat Spawner 하는 중

---
+++ Assets/Scripts/NPC/CatSpawnManager.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CatSpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private CatMovement _catPrefab;
+
+ 
+}
